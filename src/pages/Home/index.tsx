418817ba811d3da5bdf5f6b5e69301240af38d65
Artur Miguel <arturmrocha1@gmail.com>
2021-05-09 22:44:18 -0300

    feat: Gráfico de atributos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,20 @@ import styles from './styles.module.scss';
 import loldata from '../../assets/data-11.9.1-pt-br.json';
 import { ChampionDTO } from '../../dtos/ChampionDTO';
 import { useEffect, useState } from 'react';
+import { Radar, Chart } from 'react-chartjs-2';
+
+Chart.defaults.color = '#fff'
+Chart.defaults.scale.grid.display = false;
+Chart.defaults.scale.ticks.display = true;
+Chart.defaults.scale.ticks.backdropColor = 'none';
+Chart.defaults.responsive = true;
+Chart.defaults.maintainAspectRatio = false;
 
 export default function Home() {
   const [champions, setChampions] = useState([]);
   const [leftChampion, setLeftChampion] = useState(loldata.data.Ahri);
   const [rightChampion, setRightChampion] = useState(loldata.data.Zed);
+  const [chartData, setChartData] = useState({});
 
   useEffect(() => {
     const list = Object.keys(loldata.data).map((key: string) => {
@@ -14,6 +23,35 @@ export default function Home() {
     });
     setChampions(list);
   }, []);
+
+  useEffect(() => {
+    const data = {
+      labels: Object.keys(leftChampion.info).map(key => key),
+      datasets: [
+        {
+          label: leftChampion.id,
+          backgroundColor: 'rgba(179,181,198,0.2)',
+          borderColor: '#b3b5c6',
+          pointBackgroundColor: '#b3b5c6',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: '#b3b5c6',
+          data: Object.keys(leftChampion.info).map(key => leftChampion.info[key])
+        },
+        {
+          label: rightChampion.id,
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: '#ff6384',
+          pointBackgroundColor: '#ff6384',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: '#ff6384',
+          data: Object.keys(rightChampion.info).map(key => rightChampion.info[key])
+        }
+      ]
+    };
+    setChartData(data);
+  }, [leftChampion, rightChampion]);
 
   return (
     <div className={styles.container}>
@@ -29,13 +67,13 @@ export default function Home() {
         <div className={styles.centerArea}>
           <div className={styles.gridCenter}>
             <div>
-              <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${leftChampion.id}_0.jpg`} className={styles.selectedChampionImage}></img>
+              <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${leftChampion.id}_0.jpg`} className={styles.selectedChampionImageLeft}></img>
+            </div>
+            <div style={{ width: '80%', height: '100%' }}>
+              <Radar data={chartData} type='radar' />
             </div>
             <div>
-              ...
-            </div>
-            <div>
-              <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${rightChampion.id}_0.jpg`} className={styles.selectedChampionImage}></img>
+              <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${rightChampion.id}_0.jpg`} className={styles.selectedChampionImageRight}></img>
             </div>
           </div>
         </div>
